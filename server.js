9ef140654bb22e766f536beb56a09c677b397981
Adam Burmister <adam.burmister@gmail.com>
2012-03-20 17:10:56 +0000

    Updated to heroku specs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express.createServer();
+var app = express.createServer(express.logger());
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,6 +10,7 @@ app.get('/', function(req, res) {
     res.render('index.html');
 });
 
-
-// spin up server
-app.listen(8080, '127.0.0.1')
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
